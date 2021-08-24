e313b5d99b098a036f92111a1a7d611ea85099e3
Johan Sebastian Hernandez Naranjo <sebastianh.16592@gmail.com>
2021-08-23 23:24:31 -0500

    Crear un componente manualmente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public titulo: string = 'Contador App';
-  numero: number = 10;
-  base: number =5;
 
-  acumular(valor: number) {
-    this.numero += valor;
-  }
 }
