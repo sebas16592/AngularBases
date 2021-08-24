28c321f8ee16bc782411eaa6f8bb3d9584ad1075
Johan Sebastian Hernandez Naranjo <sebastianh.16592@gmail.com>
2021-08-23 23:10:22 -0500

    Tarea del contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public titulo: string = 'Contador App';
   numero: number = 10;
+  base: number =5;
 
   acumular(valor: number) {
     this.numero += valor;
