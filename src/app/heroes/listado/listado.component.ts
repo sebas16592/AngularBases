0a0d153acdedf35d7be3efde89766a5556a1ab8d
Johan Sebastian Hernandez Naranjo <sebastianh.16592@gmail.com>
2021-08-25 23:21:48 -0500

    Directiva *ngFor

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
-  
+  heroes: string[] = ['Spiderman','Ironman','Hulk', 'Thor', 'Capitan america' ];
+  heroeBorrado: string = '...';
+
+  borrarHeroe(){
+    console.log('Borrando...');    
+    this.heroeBorrado= this.heroes.shift() || '';
+    /*const index = this.heroes.indexOf('Hulk');
+    if (index > -1) {
+      this.heroes.splice(index,1);
+    }*/
+  }
 
 }
